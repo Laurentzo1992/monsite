309c4e82a1c9afc482e15886e7e77f30849cfb37
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-11-03 20:24:33 +0000

    fontend design

--- vuneem/views.py
+++ vuneem/views.py
@@ -4,4 +4,9 @@ def index(request):
 	context = {}
 	return render(request, 'vuneem/index.html', context)
 
+
+def cv(request):
+   context = {}
+   return render(request, 'vuneem/cv.html', context)
+
 # Create your views here.
